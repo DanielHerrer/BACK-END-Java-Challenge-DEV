0f21b76ab30fe95a82c8c9a53296382ee952f55a
DanielHerrer <100254429+DanielHerrer@users.noreply.github.com>
2022-04-04 15:20:39 -0300

    Feature
    (NumPrimo)

--- Clase1/src/NumPrimo.java
+++ Clase1/src/NumPrimo.java
@@ -1,10 +1,31 @@
 public class NumPrimo {
-    int num,primo;
+    int num, primo, divisor = 2;
 
     public NumPrimo(int numero){
         this.num = numero;
     }
 
-    public void
+    public void EsPrimo(){
+        if(num == 0 || num == 1 || num == 2){
+            primo = 0;
+        }
+
+        while ((primo == 0) && (divisor!=num)){
+            if(num % divisor != 0) {
+                primo = 1;
+            }
+            divisor++;
+        }
+
+        switch(primo){
+            case 0:
+                System.out.println("El numero NO es primo.");
+                break;
+            case 1:
+                System.out.println("El numero SI es primo.");
+                break;
+        }
+
+    }
 
 }
