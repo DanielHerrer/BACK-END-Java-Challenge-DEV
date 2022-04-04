e905a3f1e5f4919292e33c9ed098633584534c99
DanielHerrer <100254429+DanielHerrer@users.noreply.github.com>
2022-04-04 16:14:03 -0300

    Refactor
    (Triangulo/Optimization)

--- Clase1/src/Triangulo.java
+++ Clase1/src/Triangulo.java
@@ -1,32 +1,21 @@
 public class Triangulo {
-    private int tipo, A, B, C;
+    private int A, B, C;
 
-    public Triangulo(int A,int B,int C){
-        this.A = A;
-        this.B = B;
-        this.C = C;
+    public Triangulo(int ladoA,int ladoB,int ladoC){
+        this.A = ladoA;
+        this.B = ladoB;
+        this.C = ladoC;
     }
 
     public void QuienSoy(){
         if(A==B && B==C){
-            tipo = 1;
+            System.out.println("El triangulo es equilatero");
         }else if(A==B || B==C || A==C){
-            tipo = 2;
+            System.out.println("El triangulo es isosceles");
         }else {
-            tipo = 3;
+            System.out.println("El triangulo es escaleno");
         }
 
-        switch(tipo){
-            case 1:
-                System.out.println("El triangulo es equilatero");
-                break;
-            case 2:
-                System.out.println("El triangulo es isosceles");
-                break;
-            case 3:
-                System.out.println("El triangulo es escaleno");
-                break;
-        }
     }
 
 }
