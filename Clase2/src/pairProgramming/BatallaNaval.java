d6c2725c2a39835d65c0560e75b798075d1a7c93
DanielHerrer <100254429+DanielHerrer@users.noreply.github.com>
2022-04-05 18:20:58 -0300

    Refactor/Fix

--- Clase2/src/pairProgramming/BatallaNaval.java
+++ Clase2/src/pairProgramming/BatallaNaval.java
@@ -1,59 +1,70 @@
 package pairProgramming;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class BatallaNaval {
-    public static void  main(String[] args){
-        Scanner scan = new Scanner(System.in);
-        int x, y, x1, y2;
+    private int x;
+    private int y;
 
-        System.out.println("Bienvenido a Batalla Naval");
-        System.out.println(Por favor, ingrese las coordenadas a ejecutar su disparo.”);
-        System.out.print(“Coordenadas X: “);
-        x1 = scan.nextInt();
-        System.out.print(“Coordenadas Y: “);
-        y1 = scan.nextInt();
-
-        Int[][] mat = new mat[10][10];
-
-        llenarMat(mat);
-
-        ubicarBarquito(mat);
-
-        disparo(x1,y1);
-
-
-        public int[][] llenarMat(int[][] mat){
-            for(int i=0;i<mat.lentgh;i++){
-                for(int j=0;j<mat[i].lentgh;i++){
-                    Mat[i][j] = 0;
-                }
+    public  void ejecutarEj(){
+        Scanner sc = new Scanner(System.in);
+        int i,j;
+        char rta = 's';
+        System.out.println("De cuanto es el ancho de su tablero?");
+        i = sc.nextInt();
+        System.out.println("De cuanto es el alto de su tablero?");
+        j = sc.nextInt();
+        int[][] mat = new int[j][i];
+        mat = llenarMat(mat);
+        mat = ubicarBarquito(mat,j-1,i-1);
+        mostrarMat(mat);
+        do {
+            if(disparo()){
+                System.out.println("HAS GANADO");
+                System.exit(0);
             }
-            return mat;
-        }
+            System.out.println("Desea volver a intentar");
+            rta = sc.next().charAt(0);
+        }while(rta =='s');
+    }
 
-        public int[][] ubicarBarquito(int[][] mat){
-            Int barco = 10;
-            this.x = (int) (Math.random()* 10 +1;
-            this.y = (int) (Math.random()* 10 +1;
-            mat[x][y] = barco ;
-            return mat
-        }
-
-.
-.
-.
-.
-.
-        public void disparo(int x1, int y1){
-            if (this.x == x && this.y == y){
-                System.out.println(“Su disparo ha acertado.”);
-            } else {
-                System.out.println(“Su disparo ha fallado.”);
+    public static int[][] llenarMat(int[][] mat){
+        for(int i=0;i<mat.length;i++){
+            for(int j=0;j<mat[i].length;j++){
+                mat[i][j] = 0;
             }
         }
+        return mat;
+    }
 
+    public int[][] ubicarBarquito(int[][] mat,int i,int j){
+        int barco = (int) (Math.random() * 200 + 1);
+        this.x = (int) (Math.random() * i +1);
+        this.y = (int)(Math.random() * j +1);
+        mat[x][y] = barco ;
+        return mat;
+    }
 
+    public boolean disparo(){
+        int x,y;
+        boolean rta = false;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese las cordenadas x de su disparo");
+        x = sc.nextInt();
+        System.out.println("Ingrese las cordenadas y de su disparo");
+        y = sc.nextInt();
+        if (this.x == y && this.y == x){
+            System.out.println("Su disparo ha acertado.");
+            rta = true;
+        } else {
+            System.out.println("Su disparo ha fallado.");
+        }
+        return rta;
+    }
 
-
+    public static void mostrarMat(int[][] mat){
+        for(int arr[] : mat){
+            System.out.println(Arrays.toString(arr));
+        }
     }
 }
